b0993bdb69500dcb14655256ea3438a7c4f3f8e2
PyraXC <link3576@gmail.com>
2022-12-01 12:02:59 -0600

    Added b/w image support
    It will detect b\w images and convert them to RGB values

--- watermark.py
+++ watermark.py
@@ -8,7 +8,7 @@ def createImage(data, img, size, wtmks):
     #image = img.load()
     image = img.load()
     i = 0
-    print(len(data))
+    #print(len(data))
     for x in range(0, size[0]):
         for y in range(0, size[1]):
             if x < wtmks[0] and y < wtmks[1]:
@@ -33,17 +33,43 @@ def delLeastSigBits(num, bits):
 def returnBits(num, bits):
     return num << MAX_BIT_VALUE - bits
 
+def check_gray(img):
+    temp = img[0,0]
+    if isinstance(temp, int):
+        return True
+    else:
+        return False
+def toRGB(img):
+    image = img.load()
+    RGB = Image.new('RGB', (img.size[0], img.size[1]))
+    rgb = RGB.load()
+    for x in range(img.size[0]):
+        for y in range(img.size[1]):
+            temp = image[x,y]
+            rgb[x,y] = (temp, temp, temp)
+    #print(rgb[0,0])
+    return RGB
+
 def waterMark(img, wtmk, bits):
     imgx, imgy = img.size
     wtmkx, wtmky = wtmk.size
+    px_in = img.load()
+    px_hide = wtmk.load()
     if wtmkx > imgx or wtmky > imgy:
         print("watermark too big")
         return
+    if(check_gray(px_in)):
+        img = toRGB(img)
+        px_in = img.load()
+
+    if(check_gray(px_hide)):
+        wtmk = toRGB(wtmk)
+        px_hide = wtmk.load()
+
     temp = []
-    px_in = img.load()
-    px_hide = wtmk.load()
     for row in range(wtmkx):
         for col in range(wtmky):
+            #print(px_hide[row, col])
             r, g, b = px_hide[row, col]
             r = mostSigBits(r, bits)
             g = mostSigBits(g, bits)
@@ -83,14 +109,15 @@ def showWatermark(img, wtmks, bits):
     return createImage(temp, img, img.size, wtmks)
             
 def main():
-    wtmk = Image.open("dg_logo.png")
-    wtmk.convert("RGB")
+    wtmk = Image.open("lena_gray_512.png")
+    #wtmk.convert("RGB")
     img = Image.open("thumbnail.png")
+    #img.convert("RGB")
     bits = 3
-    #encode = waterMark(img, wtmk, bits)
-    #encode.show()
-    #decode = showWatermark(encode, wtmk.size, bits)
-    #decode.show()
+    encode = waterMark(img, wtmk, bits)
+    encode.show()
+    decode = showWatermark(encode, wtmk.size, bits)
+    decode.show()
 
 if "__main__":
     main()
